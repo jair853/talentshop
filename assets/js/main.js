302bcb798f0e8c2d26ba5de80c0f5ae40d175b9d
Zenitram26 <Ing.Zenitram26@gmail.com>
2025-04-27 22:14:21 -0500

    se crea funcion actualizar numerito

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,8 @@ function agregarAlCarrito(e) {
  
      localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
  }
+
+ function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
